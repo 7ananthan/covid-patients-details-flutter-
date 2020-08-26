b4a081c3a5d9208266990cd9e5616fcb1c4768ac
7ananthan <7ananthan@gmail.com>
2020-08-26 19:49:54 +0530

    addpatient route committed

--- lib/views/addPatients.dart
+++ lib/views/addPatients.dart
@@ -1,12 +1,30 @@
+import 'dart:convert';
+
+import 'package:covid_patient_details/models/patientModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../main.dart';
 
+Future<PatientData> createPost(String apiUrl,{Map data}) async{
+  return http.post(apiUrl,body:data).then((http.Response response){
+    
+    final int statusCode=response.statusCode;
+    
+    if(statusCode!=200)
+      {
+        throw new Exception("Exception occured");
+     }
+    print(response.body);
+    return (json.decode(response.body));
+  });
+} 
+
 class AddPatients extends StatelessWidget {
-  TextEditingController name = TextEditingController();
-  TextEditingController age = TextEditingController();
-  TextEditingController place = TextEditingController();
+  TextEditingController name_controller = TextEditingController();
+  TextEditingController age_controller = TextEditingController();
+  TextEditingController place_controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,21 +33,21 @@ class AddPatients extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           TextField(
-            controller: name,
+            controller: name_controller,
             decoration: InputDecoration(hintText: "Patient name"),
           ),
           SizedBox(
             height: 10.0,
           ),
           TextField(
-            controller: age,
+            controller: age_controller,
             decoration: InputDecoration(hintText: "Patient Age"),
         ),
           SizedBox(
             height: 10.0,
           ),
           TextField(
-            controller: place,
+            controller: place_controller,
             decoration: InputDecoration(hintText: "Patient place"),
           ),
           SizedBox(
@@ -39,7 +57,21 @@ class AddPatients extends StatelessWidget {
             child: Text(
               "Add Patient"
             ),
-            onPressed: (){
+            onPressed: ()async{
+            PatientData patientData =new PatientData(
+              name: name_controller.text,
+              age: age_controller.text,
+              place: place_controller.text
+            );
+            
+            //API Calling
+            PatientData myData =await  createPost(
+
+              "https://coviddetalsnode.herokuapp.com/patient/add",
+               data:patientData.toJson()
+
+            );
+
 
             },
           ),
